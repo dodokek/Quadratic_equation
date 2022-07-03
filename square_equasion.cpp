34c5223b4c315f594b91ad2a8d464b201ccb037e
Aleksander <57039216+DodoKek@users.noreply.github.com>
2022-07-04 00:11:16 +0300

    Update square_equasion.cpp

--- square_equasion.cpp
+++ square_equasion.cpp
@@ -16,11 +16,9 @@ namespace QuadraticEquasion
 {
     double compute_diskr(double data_arr[]);
 
-    void print_answer(double diskr, double ans_array[]);
+    void print_answer(int roots_amount, double roots_array[]);
 
-    int amount_of_answers(double diskr);
-
-    int calculate_roots_return_amount(double ans_array[], double data_arr[], double diskr);
+    int calculate_roots_return_amount(double roots_array[], double data_arr[]);
 
     void solve_quadratic_equasion(double data_arr[]);
 }
@@ -51,7 +49,7 @@ const int Debug = true;
 
 const double Accuracy = 0.000001;
 
-const int Solve_sqr_opt = 1;
+const int SolveSqrOpt = 1;
 
 const double reserved_num = 12345.6;
 
@@ -63,7 +61,7 @@ const int INF = 100000000;
 
 bool get_input(double data_arr[], int option)
 {
-    if (option == Solve_sqr_opt)
+    if (option == SolveSqrOpt)
     {
         bool is_input_correct = false;
         bool not_exit         = true;
@@ -155,93 +153,89 @@ bool is_equal(double num_1, double num_2)
     return ( fabs(num_1 - num_2) < Accuracy ) ? true : false;
 }
 
-// merge calculate_answer +
 
-
-int QuadraticEquasion::calculate_roots_return_amount(double ans_array[], double data_arr[], double diskr)
+int QuadraticEquasion::calculate_roots_return_amount(double roots_array[], double data_arr[])
 {
-    double koef_a = data_arr[0];
-    double koef_b = data_arr[1];
-    double koef_c = data_arr[2];
+    int koef_a = data_arr[0];
+    int koef_b = data_arr[1];
+    int koef_c = data_arr[2];
 
-    double x1_indx = 0;
-    double x2_indx = 1;
+    int x1_indx = 0;
+    int x2_indx = 1;
 
-    if (is_zero(is_zero(koef_a) && !is_zero(koef_b) && !is_zero(koef_c))
+    //Special cases
+
+    if (is_zero(koef_a) && !is_zero(koef_b) && !is_zero(koef_c))
     {
-        ans_array[x1_indx] = -koef_c / koef_b;
+        roots_array[x1_indx] = -koef_c / koef_b;
         return 1;
     }
 
-    else if (is_zero(koef_a) && is_zero(koef_b) && is_zero(koef_c))
+    if (is_zero(koef_a) && is_zero(koef_b) && is_zero(koef_c))
     {
         return INF;
     }
 
-    else
+    //Special cases
+
+    double diskr = (koef_b * koef_b) - (4 * koef_a * koef_c);
+
+    if (diskr > 0)
     {
-        double diskr = (koef_b * koef_b) - (4 * koef_a * koef_c);
+        roots_array[x1_indx] = ( -koef_b + sqrt(diskr)) / (2 * koef_a);
 
-        ans_array[x1_ind] = ( -koef_b + sqrt(diskr) / (2 * koef_a);
-
-        ans_array[x2_ind] = ( -koef_b - sqrt(diskr) / (2 * koef_a);
+        roots_array[x2_indx] = ( -koef_b - sqrt(diskr)) / (2 * koef_a);
 
         return 2;
     }
 
+    if (is_zero(diskr))
+    {
+        roots_array[x1_indx] = (-koef_b) / (2 * koef_a);
+
+        roots_array[x2_indx] = (-koef_b) / (2 * koef_a);
+
+        return 1;
+    }
+
     return 0;
+
 }
 
 
-void QuadraticEquasion::print_answer(double diskr, double ans_array[])
+void QuadraticEquasion::print_answer(int roots_amount, double roots_array[])
 {
-    int answers_amount = QuadraticEquasion::amount_of_answers(diskr);
+    printf("Уравнение имеет кол-во решений равное: %d \n", roots_amount);
 
-    printf("Уравнение имеет кол-во решений равное: %d \n", answers_amount);
-
-    if (answers_amount == 0)
+    if (roots_amount == 0)
     {
         printf("%s", "Решений нет.\n");
     }
 
-    else if (answers_amount == 1)
+    else if (roots_amount == 1)
     {
-        printf("Кратный корень %lg \n", ans_array[0]);
+        printf("Кратный корень %lg \n", roots_array[0]);
+    }
+
+    else if (roots_amount == INF)
+    {
+        printf("Бесконечное количество решений");
     }
 
     else
     {
-        printf("Корень %lg, Корень %lg \n", ans_array[0], ans_array[1]);
+        printf("Корень %lg, Корень %lg \n", roots_array[0], roots_array[1]);
     }
-}
-
-
-// check_special_cases
-bool QuadraticEquasion::handle_exception(double data_arr[])
-{
-    if (is_zero(data_arr[0]) && is_zero(data_arr[1]) && is_zero(data_arr[2]))
-    {
-        printf("Бесконечное количество решений");
-        return false;
-    }
-
-    return true;
 }
 
 
 void QuadraticEquasion::solve_quadratic_equasion(double data_array[])
 {
-    double diskr = 0;    //diskriminant
-    double ans_array[2] = {reserved_num, reserved_num}; //answers
+    double roots_array[2] = {0, 0};
 
-    diskr = QuadraticEquasion::compute_diskr(data_array);
+    int roots_count = QuadraticEquasion::calculate_roots_return_amount(roots_array, data_array);
 
-    // diskr_flag
-    // return root_count
-    QuadraticEquasion::calculate_answer(ans_array, data_array, diskr);
-
-    // pass root_count
-    QuadraticEquasion::print_answer(diskr, ans_array);
+    QuadraticEquasion::print_answer(roots_count, roots_array);
 }
 
 
@@ -263,26 +257,24 @@ void UnitTestQuadraticEquasion(double test_data[],
     int x1_indx = 3;
     int x2_indx = 4;
 
-    double diskr        = 0;
-    double ans_array[2] = { reserved_num, reserved_num };
+    double diskr          = 0;
+    double roots_array[2] = { reserved_num, reserved_num };
 
+    QuadraticEquasion::calculate_roots_return_amount(roots_array, test_data);
 
-    diskr = QuadraticEquasion::compute_diskr(test_data);
-
-    QuadraticEquasion::calculate_answer(ans_array, test_data, diskr);
-
-    if (   is_equal(test_data[x1_indx], ans_array[0])
-        && is_equal(test_data[x2_indx], ans_array[1]))
+    if (   is_equal(test_data[x1_indx], roots_array[0])
+        && is_equal(test_data[x2_indx], roots_array[1]))
     {
         $sg; printf("Unit test %d passed\n", num_of_test + 1);
     }
     else
     {
-        $sr; printf("Unit test not passed, with data %lg %lg %lg,"
+        $sr; printf("Unit test %d not passed, with data %lg %lg %lg,"
                     "excepted %lg %lg, received %lg %lg\n",
+                    num_of_test + 1,
                     test_data[a_indx ], test_data[b_indx ], test_data[c_indx],
                     test_data[x1_indx], test_data[x2_indx],
-                    ans_array[a_indx ], ans_array[c_indx ]);
+                    roots_array[a_indx ], roots_array[c_indx ]);
     }
 
 }
@@ -317,7 +309,7 @@ int main()
 
     fill_array_with_NAN(data_array, MAX_DATA_SIZE);
 
-    bool data_is_correct = get_input(data_array, Solve_sqr_opt);
+    bool data_is_correct = get_input(data_array, SolveSqrOpt);
 
     if (data_is_correct)
     {
